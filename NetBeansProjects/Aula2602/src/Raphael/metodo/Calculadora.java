680f861e8e30e463475b349d57b165e8b10ed3d6
Raphael.S.nascimento <149386726+Sixxtnt@users.noreply.github.com>
2025-02-26 20:11:48 -0300

    Add files via upload

---
+++ NetBeansProjects/Aula2602/src/Raphael/metodo/Calculadora.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Raphael.metodo;
+
+/**
+ *
+ * @author RAPHAEL_4822
+ */
+public class Calculadora {
+   int valor1 = 10 ;
+   int valor2 = 10 ;
+   
+   
+ void adicao(){
+
+     int resultado = valor1 + valor2;
+     System.out .println("O valor da soma é: "+ resultado);
+}
+}
---
+++ NetBeansProjects/Aula2602/src/Raphael/metodo/DigitarScanner.java
@@ -0,0 +1,20 @@
+package Raphael.metodo;
+
+import java.util.Scanner;
+
+public class DigitarScanner {
+    public static void main(String[] args) {
+        Scanner x = new Scanner (System.in);
+   
+    System.out.println("Digite o primeiro valor");
+    int valor1 = x.nextInt();
+    
+    System.out.println("Digite o Segundo valor");
+    int valor2 = x.nextInt();
+    
+    int resultado = valor1 + valor2;
+    
+        System.out.println("O valor da soma é: "+ resultado);
+    }
+    
+}
